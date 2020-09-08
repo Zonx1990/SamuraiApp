f4d6867fe2e8ade35193a6cce0918c2e76f5fc70
Zonx1990 <Kenao10@gmail.com>
2020-09-08 09:55:48 +0200

    Branch done

--- SamuraiApp.Domain/Samurai.cs
+++ SamuraiApp.Domain/Samurai.cs
@@ -10,9 +10,11 @@ namespace SamuraiApp.Domain
        {
            Quotes = new List<Quote>();
        }
+     public List<SamuraiBattle> SamuraiBattles { get; set; }
      public int Id { get; set; }
      public string Name { get; set; }
      public List<Quote> Quotes { get; set; }
      public Clan Clan { get; set; }
-   }
+     public Horse Horse { get; set; }
+    }
 }
